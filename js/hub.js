9ddb305ac844eaf88fc84b827c2a9509f9f276ab
Nils Weberruß <nw05@gmx.de>
2025-08-21 13:17:17 +0200

    new features heheheha

--- js/hub.js
+++ js/hub.js
@@ -1,52 +1,116 @@
 let pet = {};
 let faceblock = false; // to block face updates when pet is being fed or played with
+let state = 0; 
 window.onload = loadPetData();
 
 function cuddle() {
-    faceblock = true;
-    
-    
-    pet.energy -= 25;
-    if (pet.energy < 0) {
-        pet.energy = 0;
-        document.getElementById("pet-display").src = `../assets/pet/${pet.type}/sad.png`;
-    }else{
-        pet.love += 5;
-        document.getElementById("pet-display").src = `../assets/pet/${pet.type}/love.png`;
-    }
-    setTimeout(() => {
-        faceblock = false;
-    }, 1000);
+    if (faceblock === false) {
+        faceblock = true;
+        pet.energy -= 25;
+        if (state === 1){ // if sleeping penatly
+            document.getElementById("pet-display").src = `../assets/pet/${pet.type}/sad.png`;
+            pet.happiness -= 25;
+            pet.love -= 10;
+            if (pet.happiness < 0) {
+                pet.happiness = 0;
+            }
+            if (pet.love < 0) {
+                pet.love = 0;
+            }
+        }else if (state === 0){ // awake
+            // penatly if no energy and animal is not happy
+            if (pet.energy < 0 && pet.happiness < 26) {
+                pet.energy = 0;
+                pet.love -= 5;
+                if (pet.love < 0) {
+                    pet.love = 0;
+                }
+                document.getElementById("pet-display").src = `../assets/pet/${pet.type}/sad.png`;
+            }else{
+                pet.love += 5;
+                document.getElementById("pet-display").src = `../assets/pet/${pet.type}/love.png`;
+            }
+            setTimeout(() => {
+                faceblock = false;
+            }, 1000);
+        }
+    }    
 }
 
 function action1() {
-    faceblock = true;
-    document.getElementById("pet-display").src = `../assets/pet/${pet.type}/love.png`;
-    pet.hunger += 10;
-    if (pet.hunger > pet.maxHunger) {
-        pet.happiness -= 25;
-        pet.love -= 5;
-        if (pet.happiness < 0) {
-            pet.happiness = 0;
+    // food
+    if (faceblock === false) {
+        faceblock = true;
+        document.getElementById("pet-display").src = `../assets/pet/${pet.type}/love.png`;
+        pet.hunger += 10;
+        // too much food penalty
+        if (pet.hunger > pet.maxHunger) {
+            pet.happiness -= 25;
+            pet.love -= 5;
+            if (pet.happiness < 0) {
+                pet.happiness = 0;
+            }
+            if (pet.love < 0) {
+                pet.love = 0;
+            }
+            pet.hunger = pet.maxHunger;
+            document.getElementById("pet-display").src = `../assets/pet/${pet.type}/sad.png`;
         }
-        if (pet.love < 0) {
-            pet.love = 0;
+        // eating takes energy
+        energy = pet.energy - 5;
+        if (energy < 0) {
+            energy = 0;
         }
-        pet.hunger = pet.maxHunger;
-        document.getElementById("pet-display").src = `../assets/pet/${pet.type}/sad.png`;
+        // if sleeping penatly
+        if (state === 1){
+                pet.happiness -= 25;
+                pet.love -= 5;
+                if (pet.happiness < 0) {
+                    pet.happiness = 0;
+                }
+                if (pet.love < 0) {
+                    pet.love = 0;
+                }
+            }
+        // for animation
+        setTimeout(() => {
+            faceblock = false;
+            }, 1000);
     }
-    energy = pet.energy - 5;
-    if (energy < 0) {
-        energy = 0;
+}
+
+function action2() {
+    // give water
+    if (faceblock === false) {
+        faceblock = true;
+        document.getElementById("pet-display").src = `../assets/pet/${pet.type}/love.png`;
+        pet.thirst += 20;
+        // too much water penalty
+        if (pet.thirst > pet.maxThirst) {
+            pet.happiness -= 25;
+            pet.thirst = pet.maxThirst;
+            if (pet.happiness < 0) {
+                pet.happiness = 0;
+            }
+        }
+
+        // if sleeping penatly
+        if (state === 1){
+            pet.happiness -= 25;
+            pet.love -= 5;
+            if (pet.happiness < 0) {
+                pet.happiness = 0;
+            }
+            if (pet.love < 0) {
+                pet.love = 0;
+            }
+        }
+
+        // for animation
+        setTimeout(() => {
+            faceblock = false;
+        }, 1000);
     }
-    pet.happiness += 5;
-    if (pet.happiness > pet.maxHappiness) {
-        pet.happiness = pet.maxHappiness;
-    }
-    setTimeout(() => {
-        
-        faceblock = false;
-    }, 1000);
 }
 
 
@@ -64,17 +128,33 @@ setInterval(() => updateInfo(), 100);
 
 
 function updatePet() {
-    // pet gets hungrier and add energy
-    if (pet.hunger > 0) {
-        pet.hunger -= 1;
-        if (pet.hunger < 0) {
-            pet.hunger = 0;
-        }
-        pet.energy += 1;
-        if (pet.energy > pet.maxEnergy) {
-            pet.energy = pet.maxEnergy;
+
+    // time wise sleep
+    time = new Date().getHours();
+    if(time < 8 && time > 22){
+        state = 1;
+        document.getElementById("box").classList.add("sleepy");
+        document.getElementById("box").classList.remove("wakey");
+
+    }else{
+        state = 0;
+        document.getElementById("box").classList.add("wakey");
+        document.getElementById("box").classList.remove("sleepy");
+    }
+    if (state === 0) { // when awake
+        // pet gets hungrier and add energy
+        if (pet.hunger > 0) {
+            pet.hunger -= 1;
+            if (pet.hunger < 0) {
+                pet.hunger = 0;
+            }
+            pet.energy += 1;
+            if (pet.energy > pet.maxEnergy) {
+                pet.energy = pet.maxEnergy;
+            }
         }
     }
+    
     // pet gets unhappy when hungry
     if (pet.happiness > 0) {
         if (pet.hunger === 0) {
@@ -82,13 +162,16 @@ function updatePet() {
         }
     }
 
-    // pet gets thirsty
-    if (pet.thirst > 0) {
-        pet.thirst -= 2;
-        if (pet.thirst < 0) {
-            pet.thirst = 0;
+    if (state === 0){ // when awake
+        // pet gets thirsty
+        if (pet.thirst > 0) {
+            pet.thirst -= 2;
+            if (pet.thirst < 0) {
+                pet.thirst = 0;
+            }
         }
     }
+    
 
     // pet gets sad when thirsty
     if (pet.happiness > 0) {
@@ -103,6 +186,22 @@ function updatePet() {
             pet.energy = 0;
         }
     }
+    // happy if there is energy + 50% < thirst + 50% < hunger = +1 happiness
+    if (pet.energy > 0 && pet.thirst > pet.maxThirst / 2 && pet.hunger > pet.maxHunger / 2) {
+        pet.happiness += 1;
+        if (pet.happiness > pet.maxHappiness){
+            pet.happiness = pet.maxHappiness;
+        
+        }
+    }
+    // when a lot of energy and good enough food and thrist then more happy
+    if (pet.energy > 50 && pet.thirst > pet.maxThirst / 2 && pet.hunger > pet.maxHunger / 2) {
+        pet.happiness += 3;
+        if (pet.happiness > pet.maxHappiness){
+            pet.happiness = pet.maxHappiness;
+        
+        }
+    }
 }
 
 function updateInfo() {
@@ -111,7 +210,7 @@ function updateInfo() {
     document.getElementById("pet-name").innerHTML = pet.name;
     document.getElementById("pet-hunger").innerHTML = `Hunger: ${pet.hunger}/${pet.maxHunger}`;
     document.getElementById("pet-thirst").innerHTML = `Thirst: ${pet.thirst}/${pet.maxThirst}`;
-    document.getElementById("pet-happiness").innerHTML = `Happiness: ${pet.happiness}/${pet.maxHappiness}`;
+    document.getElementById("pet-happiness").innerHTML = `Happy: ${pet.happiness}/${pet.maxHappiness}`;
     document.getElementById("pet-energy").innerHTML = `Energy: ${pet.energy}/${pet.maxEnergy}`;
     document.getElementById("pet-love").innerHTML = `Love: ${pet.love}/${pet.maxLove}`;
     document.getElementById("pet-age").innerHTML = `Age: ${Math.floor(pet.age / 86400)} days`;
